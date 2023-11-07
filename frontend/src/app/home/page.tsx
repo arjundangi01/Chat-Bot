972afd5aee73e149e618862a21f752178c2dcfb5
arjundangi01 <arjundangi907473@gmail.com>
2023-11-07 13:42:31 +0530

    first commit

---
+++ frontend/src/app/home/page.tsx
@@ -0,0 +1,12 @@
+import Header from '@/components/header'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+      <Header/>
+    </div>
+  )
+}
+
+export default Page
---
+++ frontend/src/components/header.tsx
@@ -0,0 +1,12 @@
+import { AppBar,Toolbar } from '@mui/material'
+import React from 'react'
+
+const Header = () => {
+  return (
+      <AppBar>
+          <Toolbar></Toolbar>
+    </AppBar>
+  )
+}
+
+export default Header
